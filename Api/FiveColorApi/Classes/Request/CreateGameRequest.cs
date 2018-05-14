6f9ad60433f620babe4150945d07b8f9a8422650
Steve Corrado <steve.corrado@macys.com>
2018-05-14 07:41:15 -0400

    added api and database

---
+++ Api/FiveColorApi/Classes/Request/CreateGameRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FiveColorApi.Classes.Request
+{
+    public class CreateGameRequest
+    {
+        public int PlayerId { get; set; }
+        public string GameName { get; set; }
+    }
+}
